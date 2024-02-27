e1de9ca119e8e0e80b395d11679c6c14df4c2e19
Andreirl032 <andrei.ramos.lopes@gmail.com>
2024-02-26 22:39:13 -0300

    adicionada imagem e posições ajustadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <>
       <body>
         <Header />
-        <main className="flex flex-row items-start mx-36 mt-20">
-        <div className="flex flex-1 flex-col items-start gap-12">
+        <main className="flex flex-row items-start">
+        <div className="flex flex-1 flex-col items-start gap-12 ml-40 mt-20">
           <div className="font-bold leading-none text-[5rem]">
             <h1>Make</h1>
             <h1>remote work</h1>
@@ -35,8 +35,8 @@ export default function Home() {
             <Image src={maker} objectFit="contain" alt="maker" />
           </div>
         </div>
-        <div className="flex-1">
-          <Image alt="image-hero" src={imageHero} objectFit="contain"></Image>
+        <div className="flex-1 absolute right-28">
+          <Image alt="image-hero" src={imageHero} height={420} width={420}></Image>
         </div>
         </main>
       </body>
